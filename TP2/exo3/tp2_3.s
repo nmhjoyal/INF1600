8e0f47de7d320e67be3fd1aebebf88d1e36e9a4d
nmhjoyal <37188754+nmhjoyal@users.noreply.github.com>
2018-03-13 12:45:20 -0400

    Update tp2_3.s

--- TP2/exo3/tp2_3.s
+++ TP2/exo3/tp2_3.s
@@ -1,14 +1,20 @@
 .data
 	/* Votre programme assembleur ici... */
-	i:
+	i: .int 10
+	zero: .int 0
 
 .global func_s
 
 func_s:	
+	mov i, %esi
+	mov zero, %ecx
+	jmp boucle
+	
 	conditionIf2:
 		mov b, %ebx
 		sub $500, %ebx
 		mov %ebx, b
+		jmp boucle
 
 	conditionIf: 
 		mov c, %edx
@@ -17,6 +23,7 @@ func_s:
 		mov b, %ebx
 		cmp %edx, %ebx
 		ja conditionIf2
+		jmp boucle
 
 	conditionElse:
 		mov b, %ebx
@@ -25,21 +32,22 @@ func_s:
 		mov d, %edx
 		add $500, %edx
 		mov %edx, d
+		jmp boucle
 
 	boucle:
 		mov a, %eax
 		add d, %eax
 		add e, %eax
 		sub b, %eax
-	mov %eax, a 
+		mov %eax, a 
 		mov b, %ebx
 		sub $1000, %ebx
 		mov c, %edx
 		add $500, %edx
 		cmp %ebx, %edx
-		ja conditionIf
-		jna conditionElse
-		loop boucle
-
-	
-	ret
+		ja conditionIf 		# si (c+500) - (b-1000) > 0, jmp if
+		jna conditionElse	
+		add $1, %ecx
+		cmp %ecx, %esi 		
+		ja boucle		# i (%esi) > compteur (%ecx), boucle 
+		ret

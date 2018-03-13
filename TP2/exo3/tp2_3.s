904c7fc8064d8e28ea34f0fdb145c8c819faf1c0
Jennifer Girard <jenn-suki@hotmail.com>
2018-03-13 14:19:25 -0400

    modifs exo3.. encore

--- TP2/exo3/tp2_3.s
+++ TP2/exo3/tp2_3.s
@@ -3,7 +3,7 @@
 	i: .int 10
 	increm: .int 0
 	zero: .int 0
-
+	
 .global func_s
 
 func_s:	
@@ -11,50 +11,48 @@ func_s:
 	mov increm, %ecx
 	
 	boucle:
-		mov zero, %eax
-		mov d, %ebx
-		add %ebx, %eax
+		mov d, %eax
 		mov e, %ebx
-		add %ebx, %eax
+		add %ebx, %eax		# d+e
 		mov b, %ebx
-		sub %ebx, %eax
-		mov %eax, a 
-		mov b, %ebx			# pour comparer la 1ere condition
-		sub $1000, %ebx
+		sub %ebx, %eax		#(d+e)-b
+		mov %eax, a 		# a = (d+e)-b
+		mov b, %ebx		
+		sub $1000, %ebx		# b-1000
 		mov c, %edx
-		add $500, %edx
-		cmp %ebx, %edx
+		add $500, %edx		# c+500
+		cmp %ebx, %edx		# (c+500) - (b-1000)
 		ja conditionIf 		# si (c+500) - (b-1000) > 0, jmp if
-		jna conditionElse	
+		jmp conditionElse
 
 
 	conditionIf: 
-		mov c, %edx
-		sub $500, %edx
+		mov c, %edx			
+		sub $500, %edx		# c-500
 		mov %edx, c
-		mov b, %ebx
-		cmp %edx, %ebx
-		ja conditionIf2
+		mov b, %ebx		
+		cmp %edx, %ebx		# b - (c-500)
+		ja conditionIf2		# si b - (c-500) > 0
 		jmp break
 		
 	conditionIf2:
 		mov b, %ebx
-		sub $500, %ebx
+		sub $500, %ebx		# b-500
 		mov %ebx, b
 		jmp break
 
 	conditionElse:
-		mov b, %ebx
-		mov e, % eax
-		sub %eax, %ebx
+		mov b, %ebx			
+		mov e, %eax
+		sub %eax, %ebx		# b-e
 		mov %ebx, b
 		mov d, %edx
-		add $500, %edx
+		add $500, %edx		# d+500
 		mov %edx, d
 		jmp break
 		
 	break:
-		add $1, %ecx
-		cmp %ecx, %esi 		
-		jae boucle			# i (%esi) > compteur (%ecx), boucle 
+		add $1, %ecx		#incremente compteur
+		cmp %ecx, %esi 		# compteur - 10
+		jae boucle			# si 10 >= compteur, boucle 
 		ret

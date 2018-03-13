f12810b5186c0aeaa09f3d47bdc0ef6fcf5f5330
Jennifer Girard <jenn-suki@hotmail.com>
2018-03-13 12:18:11 -0400

    exo2 fait

--- TP2/exo2/tp2_2.s
+++ TP2/exo2/tp2_2.s
@@ -2,26 +2,26 @@
 
 func_s:
 	flds b
-	flds d	#b = s[1], d = s[0]
+	flds d		#b = s[1], d = s[0]
 	fmulp		#b * d = s[0]
-	flds c	#c = s[0], b*d = s[1]
-	fsubrp	#s[1] - s[0] = s[0]
-	fstps c	#résultat mis dans c
+	flds c		#c = s[0], b*d = s[1]
+	fsubrp		#s[1] - s[0] = s[0]
+	fstps a		#résultat mis dans a
 	flds f	
-	flds g	#f = s[1], g = s[0]
-	fsubrp	#s[1] - s[0] = s[0]
-	flds c	#résultat sous. = s[1], c = s[0]
+	flds g		#f = s[1], g = s[0]
+	fsubrp		#s[1] - s[0] = s[0]
+	flds a		#résultat sous. = s[1], c = s[0]
 	fdivp		# s[0] / s[1] = s[0]
-	flds e	# résultat div. = s[1], e = s[0]
+	flds e		# résultat div. = s[1], e = s[0]
 	faddp		# s[0] + s[1] = s[0]
-	fstps c	# s[0] mis dans c
+	fstps a		# s[0] mis dans a
 	flds e	
-	flds g	# e = s[1], g = s[0]
+	flds g		# e = s[1], g = s[0]
 	fsubp		# s[0] - s[1] = s[0]
-	flds f	# résultat = st[1], f = st[0]
-	fdivrp	# st[1] / st[0] = st[0]
-	flds c	# résultat = st[1], c = st[0]
+	flds f		# résultat = st[1], f = st[0]
+	fdivrp		# st[1] / st[0] = st[0]
+	flds a		# résultat = st[1], c = st[0]
 	fmulp		# st[0] * st[1] = st[0]
-	fstps a	# met st[0] dans a
+	fstps a		# met st[0] dans a
 
 	ret

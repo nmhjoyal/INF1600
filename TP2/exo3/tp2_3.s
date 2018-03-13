0e7b0b1a6e321adcf37ec5cf2a5dd37bbb6fb37b
nmhjoyal <37188754+nmhjoyal@users.noreply.github.com>
2018-03-13 11:23:16 -0400

    code assembleur exo3, func_s

--- TP2/exo3/tp2_3.s
+++ TP2/exo3/tp2_3.s
@@ -5,6 +5,41 @@
 .global func_s
 
 func_s:	
-	/* Votre programme assembleur ici... */
+	conditionIf2:
+		mov b, %ebx
+		sub $500, %ebx
+		mov %ebx, b
+
+	conditionIf: 
+		mov c, %edx
+		sub $500, %edx
+		mov %edx, c
+		mov b, %ebx
+		cmp %edx, %ebx
+		ja conditionIf2
+
+	conditionElse:
+		mov b, %ebx
+		sub e, %ebx
+		mov %ebx, b
+		mov d, %edx
+		add $500, %edx
+		mov %edx, d
+
+	boucle:
+		mov a, %eax
+		add d, %eax
+		add e, %eax
+		sub b, %eax
+	mov %eax, a 
+		mov b, %ebx
+		sub $1000, %ebx
+		mov c, %edx
+		add $500, %edx
+		cmp %ebx, %edx
+		ja conditionIf
+		jna conditionElse
+		loop boucle
+
 	
 	ret

32614284f52807806c7731a61fd7c499e2f3e4f5
Nicole <nijoya@polymtl.ca>
2018-04-11 20:24:11 -0400

    code encore plus optimize

--- TP5/tp5.c
+++ TP5/tp5.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 unsigned int Decryption_fct(unsigned int le)
@@ -14,34 +13,11 @@ unsigned int Decryption_fct(unsigned int le)
 	
 
 	asm volatile (
-		/*"movl $0xff000000, %%ebx\n" 		//mov '0xff000000' dans ebx	
-		"movl %1, %%eax\n" 					//mov 'le' (%1) dans %eax
-		"and %%ebx, %%eax\n"				//'ET' logique 
-		"movl %%eax, %%ecx\n"				// mov resultat dans ecx
-		"movl %1, %%eax\n"				
-		"movl $0xff, %%ebx\n"
-		"and %%ebx, %%eax\n"				
-		"shl $16, %%eax\n"					//shift gauche de 16 bits
-		"or %%ecx, %%eax\n"					//'OU' logique avec resultat precedent 12(%ebp)
-		"movl %%eax, %%ecx\n"
-		"movl %1, %%eax\n"
-		"movl $0xff00, %%ebx\n"
-		"and %%ebx, %%eax\n"
-		"or %%ecx, %%eax\n"
-		"movl %%eax, %%ecx\n"
-		"movl %1, %%eax\n"
-		"movl $0xff0000, %%ebx\n"
-		"and %%ebx, %%eax\n"
-		"shr $16, %%eax\n"
-		"or %%ecx, %%eax\n"
-		"movl %%eax, %0\n"*/				//mettre resultat dans 'be' (%0)
-		"movl %1, %%eax\n"
-		"bswap %%eax\n"
-		"ror $8, %%eax\n"
-		"movl %%eax, %0\n"
+		"bswap %1\n"
+		"ror $8, %1\n"
 		: "=r"(be)
 		: "r"(le)
-		: "%eax"
+		: 
 	);
 
 	return be;
--- TP5/tp5.s
+++ TP5/tp5.s
@@ -5,33 +5,30 @@
 Decryption_fct:
 .LFB0:
 	.cfi_startproc
-	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
-	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
-	subl	$16, %esp
-	movl	8(%ebp), %edx
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movl	-20(%rbp), %eax
 #APP
-# 16 "tp5.c" 1
-	movl %edx, %eax
-bswap %eax
+# 15 "tp5.c" 1
+	bswap %eax
 ror $8, %eax
-movl %eax, %edx
 
 # 0 "" 2
 #NO_APP
-	movl	%edx, -4(%ebp)
-	movl	-4(%ebp), %eax
-	leave
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
 	.size	Decryption_fct, .-Decryption_fct
 	.section	.rodata
-	.align 4
+	.align 8
 .LC0:
 	.string	"Repr\303\251sentation crypte en little-endian:   %08x\nRepr\303\251sentation decrypte en big-endian:    %08x\n"
 	.text
@@ -40,36 +37,28 @@ movl %eax, %edx
 main:
 .LFB1:
 	.cfi_startproc
-	leal	4(%esp), %ecx
-	.cfi_def_cfa 1, 0
-	andl	$-16, %esp
-	pushl	-4(%ecx)
-	pushl	%ebp
-	.cfi_escape 0x10,0x5,0x2,0x75,0
-	movl	%esp, %ebp
-	pushl	%ecx
-	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
-	subl	$20, %esp
-	movl	$-290800641, -12(%ebp)
-	pushl	-12(%ebp)
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$-290800641, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
 	call	Decryption_fct
-	addl	$4, %esp
-	subl	$4, %esp
-	pushl	%eax
-	pushl	-12(%ebp)
-	pushl	$.LC0
-	call	printf
-	addl	$16, %esp
+	movl	%eax, %edx
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
 	movl	$0, %eax
-	movl	-4(%ebp), %ecx
-	.cfi_def_cfa 1, 0
+	call	printf
+	movl	$0, %eax
 	leave
-	.cfi_restore 5
-	leal	-4(%ecx), %esp
-	.cfi_def_cfa 4, 4
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE1:
 	.size	main, .-main
-	.ident	"GCC: (GNU) 7.2.1 20170915 (Red Hat 7.2.1-2)"
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
 	.section	.note.GNU-stack,"",@progbits
